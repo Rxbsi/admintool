01f116425867e7f851abfe4292600d10797d481f
Rxbsi <robinfranz02@gmail.com>
2025-01-20 21:00:19 +0100

    Add ListAllUsers

--- backend/src/main/java/de/rxbsi/admintool/controller/UserController.java
+++ backend/src/main/java/de/rxbsi/admintool/controller/UserController.java
@@ -1,12 +1,13 @@
 package de.rxbsi.admintool.controller;
 
 import de.rxbsi.admintool.dto.User;
+import de.rxbsi.admintool.model.UserEntity;
+import de.rxbsi.admintool.repository.UserRepository;
 import de.rxbsi.admintool.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -14,10 +15,17 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/create")
     User createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping("/users")
+    List<UserEntity> listAllUsers() {
+        return userRepository.findAll();
     }
 
 
